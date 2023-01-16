3abdf7d5d232e795fc6b23638f3bccce8d7e0dc4
Maks2712 <maks2712@yahoo.com>
2023-01-16 20:59:52 +0700

    add search to flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,11 @@ class FlightsController < ApplicationController
   def index
     @flights=Flight.all
   end
+  def search
+   #@flight_results= Flight.find(flight_duration: "20:00")
+  end
+private
+  def event_date_formatted
+    event_date.strftime("%m/%d/%Y")
+  end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,19 @@
 class Flight < ApplicationRecord
     belongs_to :departure_airport, class_name: 'Airport', foreign_key: :departure_airport_id
     belongs_to :arrival_airport, class_name: 'Airport', foreign_key: :arrival_airport_id
+
+    def self.search(search)
+        if search
+            flight=Flight.find_by(name: search)
+            if flight
+                self.where(flight_id: flight)
+            else
+                @flights=Flight.all
+            end
+        else
+            @flights=Flight.all
+    end
+    
+   #   <%= select_tag "event_date", options_from_collection_for_select(UgradRsvp.get_event_dates, 'event_date', :event_date_formatted) %>
+      
 end
